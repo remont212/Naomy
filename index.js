a82393e417c1413ff325b6f7c62fed67a8d938a0
remont212 <107880286+remont212@users.noreply.github.com>
2022-09-06 17:48:37 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,139 @@
+new Vue({
+    el: ".settings",
+    data: {
+  name: ""   
+    }
+});
+
+$("#red").on("click", function(){
+    $("textarea").css({
+        "color":"red"
+    })
+});
+$("#yellow").on("click", function(){
+    $("textarea").css({
+        "color":"yellow"
+    })
+});
+$("#green").on("click", function(){
+    $("textarea").css({
+        "color":"green"
+    })
+});
+$("#black").on("click", function(){
+    $("textarea").css({
+        "color":"#1a1a1a"
+    })
+});
+$("#blue").on("click", function(){
+    $("textarea").css({
+        "color":"blue"
+    })
+});
+$("#pink").on("click", function(){
+    $("textarea").css({
+        "color":"pink"
+    })
+});
+$("#orange").on("click", function(){
+    $("textarea").css({
+        "color":"orange"
+    })
+});
+
+$("#txt1").on("click", function(){
+    $("textarea").css({
+        "font-size":"12px"
+    })
+});
+
+$("#txt2").on("click", function(){
+    $("textarea").css({
+        "font-size":"18px"
+    })
+});
+
+$("#txt3").on("click", function(){
+    $("textarea").css({
+        "font-size":"24px"
+    })
+});
+
+$("#txt4").on("click", function(){
+    $("textarea").css({
+        "font-size":"30px"
+    })
+});
+
+$("#txt5").on("click", function(){
+    $("textarea").css({
+        "font-size":"36px"
+    })
+});
+
+$("#txt6").on("click", function(){
+    $("textarea").css({
+        "font-size":"48px"
+    })
+});
+
+$("#txt7").on("click", function(){
+    $("textarea").css({
+        "font-size":"56px"
+    })
+});
+
+$("#txt8").on("click", function(){
+    $("textarea").css({
+        "font-size":"60px"
+    })
+});
+
+$("#txt9").on("click", function(){
+    $("textarea").css({
+        "font-size":"67px"
+    })
+});
+
+$("#txt10").on("click", function(){
+    $("textarea").css({
+        "font-size":"72px"
+    })
+});
+
+$("#bgred").on("click", function(){
+    $("textarea").css({
+        "background-color":"red"
+    })
+});
+$("#bgyellow").on("click", function(){
+    $("textarea").css({
+        "background-color":"yellow"
+    })
+});
+$("#bggreen").on("click", function(){
+    $("textarea").css({
+        "background-color":"green"
+    })
+});
+$("#bgblack").on("click", function(){
+    $("textarea").css({
+        "background-color":"#1a1a1a"
+    })
+});
+$("#bgblue").on("click", function(){
+    $("textarea").css({
+        "background-color":"blue"
+    })
+});
+$("#bgpink").on("click", function(){
+    $("textarea").css({
+        "background-color":"pink"
+    })
+});
+$("#bgorange").on("click", function(){
+    $("textarea").css({
+        "background-color":"orange"
+    })
+});
+
